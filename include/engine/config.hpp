73b95645443b6d2caa0b9e1d0208ec27ee4a1376
saalik <novacastor8899@gmail.com>
2026-07-09 12:10:02 +0530

    Fixed hardcoded home directory

--- include/engine/config.hpp
+++ include/engine/config.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
+#include <cstdlib> 
+#include <string>
+#include <iostream>
+
+
 namespace Engine {
+    inline std::string get_home_directory() {
+        if (const char* home_env = std::getenv("HOME")) {
+           return std::string(home_env); 
+        } else {
+            std::cerr << "Warning: Could not find HOME directory, defaulting to root.\n";
+            return "/"; 
+        }
+    }
     struct Config{
-        std::string database = "test.db";
-        std::string root_directory = "/home/salik";
+        std::string database = "search_engine.db";
+        std::string root_directory = get_home_directory();
     };
 }
